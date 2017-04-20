4a7a19b5d50fa9c9fd64040c8d8fc554621e433a
xiangflight <xiangflight@foxmail.com>
2017-04-20 16:08:07 +0800

    init project

---
+++ app/src/main/java/com/example/xiang/androidnotes/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.xiang.androidnotes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
